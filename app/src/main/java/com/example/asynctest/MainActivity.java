8384a8b0b649168d74bcbb865416d041e09946f2
yeeun kim <kye24601@gmail.com>
2021-03-21 21:17:19 +0900

    add comment

--- app/src/main/java/com/example/asynctest/MainActivity.java
+++ app/src/main/java/com/example/asynctest/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 } else {
                     value++;
-                    publishProgress(value);
+                    publishProgress(value); //call onProgressUpdate
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
